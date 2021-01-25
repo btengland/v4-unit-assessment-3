4f404cf1c741322e40a16477259082371e94df97
Brady England <bradyeng22@gmail.com>
2021-01-25 15:15:46 -0700

    Almost done

--- closures/practice-closures.js
+++ closures/practice-closures.js
@@ -9,12 +9,20 @@
   */
   
   //CODE HERE
+myFunc = () => {
+  let myStr = 'super secret string';
+  
+  getSecretString = () => {
+    return myStr
+  };
 
+  return getSecretString;
+}
   
   //Now create a variable called secretString. Its value should be the invocation of myFunc.
 
   //CODE HERE
-  
+  const secretString = myFunc()
   
   ////////////PROBLEM 2////////////
 
@@ -29,28 +37,40 @@
   */
   
   //CODE HERE
+lightSwitch = () => {
+  let isTheLightOn = false
 
+  flipTheSwitch = () => {
+    isTheLightOn = !isTheLightOn
+    if(isTheLightOn === true){
+      return `The light is on`
+    } else {
+      return `The light is off`
+    }
+  }
+  return flipTheSwitch;
+}
   
   //Create a variable called kitchenSwitch whose value is the invocation of lightSwitch.
   
   //CODE HERE
-
+const kitchenSwitch = lightSwitch()
   
   //Invoke kitchenSwitch.
 
   //CODE HERE
-  
+kitchenSwitch()
   
   //Create a variable called bathroomSwitch whose value is the invocation of lightSwitch. 
 
   //CODE HERE
-  
+const bathroomSwitch = lightSwitch()
   
   //Invoke bathroomSwitch twice.
   
   //CODE HERE
-
-  
+bathroomSwitch()
+bathroomSwitch()  
   
   ////////////PROBLEM 3////////////
 
@@ -64,7 +84,21 @@
   */
 
   //CODE HERE
-
+  plantTracker = () => {
+    plant = 'fern',
+    height = 12
+    return{
+      readInfo(){
+        return (`This is a ${plant} plant that is ${height} inches tall.`)
+      },
+      waterPlant(){
+        return height += 1
+      },
+      prunePlant(){
+        return height -= 1
+      }
+    }
+  }
 
   ////////////PROBLEM 4////////////
 
@@ -79,18 +113,31 @@
   */
 
   //CODE HERE
-
+inventory = () => {
+  let products = []
+  return {
+    readProducts(){
+      return products
+    },
+    addToProducts(string){
+      return products.push(string)
+    },
+    deleteFromProducts(string){
+      return products.splice(products[string], 1)
+    }
+  }
+}
 
   /*
     Create a variable called 'shoes' whose value is the invocation of inventory.
   */
 
   //CODE HERE
-
+const shoes = inventory()
 
   /*
     Add an item to your shoes array using the addToProducts function
   */
 
   //CODE HERE
-
+shoes.addToProducts()
